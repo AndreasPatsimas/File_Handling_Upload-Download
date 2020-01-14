267be195fc484b4be921a4dbf0f77771ad2c0c31
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-01-14 16:26:17 +0200

    Update TestExample.java

--- src/main/java/org/patsimas/file/utils/TestExample.java
+++ src/main/java/org/patsimas/file/utils/TestExample.java
@@ -5,8 +5,14 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.patsimas.file.utils.MyRemoteFileUtils.*;
 
@@ -21,6 +27,31 @@ public class TestExample {
 //        deleteRemoteFilesWithSamePrefixOrSuffix(channelSftp, "/tmp/af/", "dailyReport_08.*\\.txt");
 //
 //        closeSFTPConnection(session);
+
+//        List<File> files = Files.list(new File("C:/Directory1/ge").toPath()).map(Path::toFile)
+//                .collect(Collectors.toList());
+
+        //files.forEach(file -> System.out.println(file.getAbsolutePath()));
+
+        File fileDir = new File("C:/Directory1");
+        File fileGe = new File("C:/Directory1/ge");
+
+
+        List<File> filesGe = Arrays.asList(fileGe.listFiles());
+
+        List<String> filesDir = Arrays.asList(fileDir.list());
+
+        filesGe.forEach(file -> {
+
+            if(!filesDir.contains(file.getName()))
+                System.out.println(file.getAbsolutePath());
+        });
+
+
+        //boolean b = files.contains(new File("C:/Directory1/ap.txt"));
+
+        //System.out.println(b);
+
     }
 
 }
