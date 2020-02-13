9637517a2d5ba6db503a4972e8aed587eb47c020
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-02-13 16:04:09 +0200

    Update MyAwsFileUtils.java

--- src/main/java/org/patsimas/file/utils/MyAwsFileUtils.java
+++ src/main/java/org/patsimas/file/utils/MyAwsFileUtils.java
@@ -1,13 +1,10 @@
 package org.patsimas.file.utils;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
