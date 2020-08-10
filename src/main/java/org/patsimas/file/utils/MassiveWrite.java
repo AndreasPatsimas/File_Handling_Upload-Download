0cf36a105960ee5db38c65b7a9b3b7c6a157d3dc
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-08-10 19:18:39 +0300

    Update MassiveWrite.java

--- src/main/java/org/patsimas/file/utils/MassiveWrite.java
+++ src/main/java/org/patsimas/file/utils/MassiveWrite.java
@@ -1,13 +1,40 @@
 package org.patsimas.file.utils;
 
+//https://www.journaldev.com/881/java-append-to-file
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+@Slf4j
 public class MassiveWrite {
 
     public static void main(String[] args){
 
-
+        massiveWriteColumns(700, "C:/Directory1/help.txt");
     }
 
-    private static void massiveWriteToFile(){
-        
+    private static void massiveWriteColumns(int lines, String path){
+
+        try{
+            File file = new File(path);
+            FileWriter fr = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            log.info("writing...");
+            for (int i = 1; i <= lines; i++){
+                br.write("@Column(name = \"a" + i + "\", nullable = false)\n" +
+                        "private String a" + i + ";");
+                br.newLine();br.newLine();
+            }
+
+            br.close();
+            fr.close();
+
+            log.info("completed");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
