bd0e184deb05b9c5481c91d5e01b9e66d75e2f26
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-08-11 12:13:31 +0300

    up

--- src/main/java/org/patsimas/file/utils/MassiveWrite.java
+++ src/main/java/org/patsimas/file/utils/MassiveWrite.java
@@ -12,7 +12,10 @@ public class MassiveWrite {
 
     public static void main(String[] args){
 
-        massiveWriteColumns(700, "C:/Directory1/help.txt");
+        //massiveWriteColumns(700, "C:/Directory1/entities.txt");
+        //massiveWriteConverters(700, "C:/Directory1/converters.txt");
+        //massiveWriteDtos(700, "C:/Directory1/dtos.txt");
+        massiveWriteTestBuilders(700, "C:/Directory1/test_builders.txt");
     }
 
     private static void massiveWriteColumns(int lines, String path){
@@ -26,6 +29,71 @@ public class MassiveWrite {
                 br.write("@Column(name = \"a" + i + "\", nullable = false)\n" +
                         "private String a" + i + ";");
                 br.newLine();br.newLine();
+            }
+
+            br.close();
+            fr.close();
+
+            log.info("completed");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void massiveWriteConverters(int lines, String path){
+
+        try{
+            File file = new File(path);
+            FileWriter fr = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            log.info("writing...");
+            for (int i = 1; i <= lines; i++){
+                br.write(".a" + i + "(entities.getA" + i + "())");
+                br.newLine();
+            }
+
+            br.close();
+            fr.close();
+
+            log.info("completed");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void massiveWriteDtos(int lines, String path){
+
+        try{
+            File file = new File(path);
+            FileWriter fr = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            log.info("writing...");
+            for (int i = 1; i <= lines; i++){
+                br.write("private String a" + i + ";");
+                br.newLine();br.newLine();
+            }
+
+            br.close();
+            fr.close();
+
+            log.info("completed");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void massiveWriteTestBuilders(int lines, String path){
+        try{
+            File file = new File(path);
+            FileWriter fr = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            log.info("writing...");
+            for (int i = 1; i <= lines; i++){
+                br.write(".a" + i + "(\"DUMMY\")");
+                br.newLine();
             }
 
             br.close();
