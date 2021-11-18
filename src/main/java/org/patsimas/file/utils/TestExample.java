86285620167bd2a0080c34c8403ea054bcadf0db
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2021-11-18 22:16:45 +0200

    export excel

--- src/main/java/org/patsimas/file/utils/TestExample.java
+++ src/main/java/org/patsimas/file/utils/TestExample.java
@@ -1,9 +1,39 @@
 package org.patsimas.file.utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class TestExample {
 
     public static void main(String[] args) throws Exception {
 
-
+        //Instantiating the File class
+        String filePath = "C:/data/Επιστολή ΕΙΣΠΡΑΚΤΕΟΥ ποσού.doc";
+        File file = new File(filePath);
+        //Instantiating the Scanner class to read the file
+        Scanner sc = new Scanner(file);
+        //instantiating the StringBuffer class
+        StringBuffer buffer = new StringBuffer();
+        //Reading lines of the file and appending them to StringBuffer
+        while (sc.hasNextLine()) {
+            String x = sc.nextLine();
+            System.out.println(x);
+            buffer.append(x);
+        }
+        String fileContents = buffer.toString();
+        System.out.println("Contents of the file: "+ fileContents);
+        //closing the Scanner object
+        sc.close();
+        String oldLine = "<<customerName>>";
+        String newLine = "ΠΑΕ ΑΡΗΣ";
+        //Replacing the old line with new line
+        fileContents = fileContents.replaceAll(oldLine, newLine);
+        //instantiating the FileWriter class
+        FileWriter writer = new FileWriter(filePath);
+        System.out.println("");
+        System.out.println("new data: "+ fileContents);
+        writer.append(fileContents);
+        writer.flush();
     }
 }
